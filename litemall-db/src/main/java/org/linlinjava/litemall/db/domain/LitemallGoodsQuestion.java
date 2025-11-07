18aa88e43f0adac27911165ed61d95901096a70a
抱着羊啃 <1437670973@qq.com>
2025-11-07 10:12:54 +0800

    1

---
+++ litemall-db/src/main/java/org/linlinjava/litemall/db/domain/LitemallGoodsQuestion.java
@@ -0,0 +1,99 @@
+package org.linlinjava.litemall.db.domain;
+
+import java.time.LocalDateTime;
+
+public class LitemallGoodsQuestion {
+    public static final Boolean IS_DELETED = Deleted.IS_DELETED.value();
+    public static final Boolean NOT_DELETED = Deleted.NOT_DELETED.value();
+
+    private Integer id;
+    private Integer goodsId;
+    private Integer userId;
+    private String question;
+    private String answer;
+    private Integer answerUserId;
+    private LocalDateTime answerTime;
+    private LocalDateTime addTime;
+    private LocalDateTime updateTime;
+    private Boolean deleted;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(Integer goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public Integer getAnswerUserId() {
+        return answerUserId;
+    }
+
+    public void setAnswerUserId(Integer answerUserId) {
+        this.answerUserId = answerUserId;
+    }
+
+    public LocalDateTime getAnswerTime() {
+        return answerTime;
+    }
+
+    public void setAnswerTime(LocalDateTime answerTime) {
+        this.answerTime = answerTime;
+    }
+
+    public LocalDateTime getAddTime() {
+        return addTime;
+    }
+
+    public void setAddTime(LocalDateTime addTime) {
+        this.addTime = addTime;
+    }
+
+    public LocalDateTime getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(LocalDateTime updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Boolean getDeleted() {
+        return deleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        this.deleted = deleted;
+    }
+}
